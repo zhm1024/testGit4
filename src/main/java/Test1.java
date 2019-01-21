7716992ce0e0222295a6ec6e41199299416d0115
zhangmin <1214348423@qq.com>
2019-01-21 20:57:56 +0800

    commit from test5 in master

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,6 +3,5 @@ public class Test1 {
         System.out.println("这是第三个测试");
         System.out.println("这是第二个测试");
         System.out.println("这是第一个测试");
-        System.out.println("hot_fix2");
     }
 }
