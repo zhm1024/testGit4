1617b3867dc408ab8e2299687b98b83b2d727c0d
zhangmin <1214348423@qq.com>
2019-01-21 17:10:15 +0800

    Merge branch 'master' of https://github.com/zhm1024/testGit4
    # Conflicts:
    #	.idea/vcs.xml
    #	src/main/java/Test1.java

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是第三个测试");
         System.out.println("这是第二个测试");
+        System.out.println("这是第一个测试");
     }
 }
